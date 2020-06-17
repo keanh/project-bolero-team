425609d5e557fae817e17c72796cbb8214482b8c
keanh <keanh275@gmail.com>
2020-06-17 16:50:37 +0700

    add date

--- src/main/java/com/bolero/boleroteam/Controller/SongRestController.java
+++ src/main/java/com/bolero/boleroteam/Controller/SongRestController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 @RestController
 public class SongRestController {
     @Autowired
@@ -18,6 +22,12 @@ public class SongRestController {
 
     @PostMapping(value = "song/create",produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createSong(@RequestBody Song song){
+        LocalDateTime now = LocalDateTime.now();
+//        System.out.println("Before Formatting: " + now);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formatDateTime = now.format(format);
+//        System.out.println("After Formatting: " + formatDateTime);
+        song.setDateSubmitted(formatDateTime);
         songService.save(song);
         HttpHeaders headers = new HttpHeaders();
         return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
