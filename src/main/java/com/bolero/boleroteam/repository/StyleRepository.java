d825dda8630a3f9361e7f724030f46153aaafe7f
MrEverything <nampp1204@gmail.com>
2020-06-19 17:50:16 +0700

    add

--- src/main/java/com/bolero/boleroteam/repository/StyleRepository.java
+++ src/main/java/com/bolero/boleroteam/repository/StyleRepository.java
@@ -2,7 +2,9 @@ package com.bolero.boleroteam.Repository;
 
 import com.bolero.boleroteam.model.Style;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StyleRepository extends CrudRepository<Style,Long> {
 
 }
