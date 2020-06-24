791a8619d69dbca77f6982a07a60c9c74b2c968b
hien <hien07072001@gmail.com>
2020-06-24 17:20:47 +0700

    mer

--- src/main/java/com/bolero/boleroteam/repository/SongRepository.java
+++ src/main/java/com/bolero/boleroteam/repository/SongRepository.java
@@ -1,6 +1,7 @@
-package com.bolero.boleroteam.repository;
+package com.bolero.boleroteam.Repository;
 
 import com.bolero.boleroteam.model.Song;
+import com.bolero.boleroteam.model.Style;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,6 +9,4 @@ import java.util.List;
 public interface SongRepository extends CrudRepository<Song, Long> {
     List<Song> findByLyricsContaining(String lyrics);
     List<Song> findByNameContaining(String lyrics);
-    List<Song> findByOrderByDateSubmittedDesc();
-    List<Song> findTop3ByOrderByDateSubmittedDesc();
 }
--- src/main/java/com/bolero/boleroteam/repository/StyleRepository.java
+++ src/main/java/com/bolero/boleroteam/repository/StyleRepository.java
@@ -1,9 +1,7 @@
-package com.bolero.boleroteam.repository;
+package com.bolero.boleroteam.Repository;
 
 import com.bolero.boleroteam.model.Style;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface StyleRepository extends CrudRepository<Style,Long> {
 }
