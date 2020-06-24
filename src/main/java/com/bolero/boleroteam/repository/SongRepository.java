d1d32acf8d6f1d4cebefebf054807fb2bca1f46b
hien <hien07072001@gmail.com>
2020-06-24 17:29:20 +0700

    mer

---
+++ src/main/java/com/bolero/boleroteam/repository/SongRepository.java
@@ -0,0 +1,13 @@
+package com.bolero.boleroteam.repository;
+
+import com.bolero.boleroteam.model.Song;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface SongRepository extends CrudRepository<Song, Long> {
+    List<Song> findByLyricsContaining(String lyrics);
+    List<Song> findByNameContaining(String lyrics);
+    List<Song> findTop3ByOrderByDateSubmittedDesc();
+    List<Song> findAllByOrderByViewsDesc();
+}
