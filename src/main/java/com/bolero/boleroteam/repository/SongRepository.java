26b4e28cfb1372869d46c93467677be329e8c76d
keanh <keanh275@gmail.com>
2020-06-25 09:22:46 +0700

    fix repository

--- src/main/java/com/bolero/boleroteam/repository/SongRepository.java
+++ src/main/java/com/bolero/boleroteam/repository/SongRepository.java
@@ -2,6 +2,7 @@ package com.bolero.boleroteam.Repository;
 
 import com.bolero.boleroteam.model.Likes;
 import com.bolero.boleroteam.model.Song;
+import com.bolero.boleroteam.model.Style;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,7 +10,6 @@ import java.util.List;
 public interface SongRepository extends CrudRepository<Song, Long> {
     List<Song> findByLyricsContaining(String lyrics);
     List<Song> findByNameContaining(String lyrics);
-    List<Song> findByOrderByDateSubmittedDesc();
     List<Song> findTop3ByOrderByDateSubmittedDesc();
     List<Likes> findAllByUserId(Long id);
 }
